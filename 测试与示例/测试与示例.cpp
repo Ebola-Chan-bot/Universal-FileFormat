73431676d74c6f339cc2643baf6e39c702c47ca2
埃博拉酱 <vhtmfu@outlook.com>
2024-12-16 22:20:45 +0800

    20241216

--- 测试与示例/测试与示例.cpp
+++ 测试与示例/测试与示例.cpp
@@ -1,9 +1,8 @@
 ﻿#include <CppUnitTest.h>
 #include <万能文件格式.hpp>
-#ifdef __cpp_lib_modules
-import std;
-#else
+#ifndef __cpp_lib_modules
 #include <queue>
+#include <fstream>
 #endif
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -13,11 +12,26 @@ namespace 测试与示例
 	TEST_CLASS(测试与示例)
 	{
 	public:
-		//目前仅支持Windows
-		TEST_METHOD(在磁盘上创建文件)
+		//使用文件（指针）分配器在磁盘上创建文件并写出数据
+		TEST_METHOD(创建磁盘文件)
 		{
-			//此方法在磁盘上创建一个文件。为0号块分配空间并写入数据。自动分配一个块，并写入数据。
-			万能文件格式::独占文件内存资源 文件("test.bin");
+			const HANDLE 文件句柄 = CreateFileA("文件.bin", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			万能文件格式::文件分配器 分配器(文件句柄);
+			分配器.初始化();
+			分配器.分配(0, sizeof(int));
+			*(int*)分配器.取指针(0) = 42;
+
+			//可以在分配器析构之前关闭文件句柄，不影响分配器正常析构。
+			CloseHandle(文件句柄);
+		}
+		//使用流式分配器读入磁盘上的文件
+		TEST_METHOD(读取磁盘文件)
+		{
+			std::fstream 文件("文件.bin", std::ios::in | std::ios::out | std::ios::binary);
+			万能文件格式::流式分配器 分配器(文件);
+			int 缓冲区;
+			分配器.读入(0, &缓冲区, sizeof(int));
+			Assert::AreEqual(42, 缓冲区);
 		}
 	};
 }
